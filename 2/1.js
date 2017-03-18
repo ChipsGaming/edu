55e9aef49ff5d910bed537555dfdc53e5587cab5
artur <Artur-Mamedbekov@yandex.ru>
2017-03-18 12:24:07 +0300

    Fix

---
+++ 2/1.js
@@ -0,0 +1,20 @@
+// Ветвления
+var condition = 5 > 2;
+
+if(condition){ // Условие
+  console.log('yeeees'); // Тело
+}
+
+if(condition){
+  console.log('yeeees');
+}
+else{ // Иначе
+  console.log('noup');
+}
+
+if(condition){
+  console.log('yeees');
+}
+else if(!condition){ // Иначе если
+  console.log('yeees');
+}
---
+++ 2/2.js
@@ -0,0 +1,11 @@
+// Напиши логику вычисления x для уравнения: ax2 + bx + c = 0
+
+// Вот формула: 
+// http://www.grandars.ru/student/vysshaya-matematika/kvadratnoe-uravnenie.html
+
+// Вот значения:
+var a = 5;
+var b = 2;
+var c = -3;
+
+// Тут напиши логику вычисления x1 и x2
